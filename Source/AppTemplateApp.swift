9271c38bc0aa79fef3768fb3dbeeb4c875d33064
Ben Thomas <support@thomases.id.au>
2023-08-23 13:29:13 +1000

    Add Xcode project

---
+++ Source/AppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTemplateApp.swift
+//  AppTemplate
+//
+//  Created by Ben Thomas on 23/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
